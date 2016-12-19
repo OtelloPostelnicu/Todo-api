c0a5d22c23086d9cf9f8fcbc3efb63b90566cac9
Otello <otello.hanara@gmail.com>
2016-12-19 17:32:10 +0200

    Add ?completed filter

--- server.js
+++ server.js
@@ -31,9 +31,18 @@ app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false});
+    };
+    
+    res.json(filteredTodos);
 });
 
 // GET /todos/:id
---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+//var person = {
+//    name: 'Otello',
+//    age: 26
+//};
+//
+//function updatePerson (obj) {
+//    obj.age = 20;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+// Array Example
+
+var grades = [15, 37];
+
+function addGrade (obj) {
+    obj.push(24);
+    debugger;
+}
+
+addGrade(grades);
+console.log(grades);
