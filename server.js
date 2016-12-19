1795328e688522c656e9c0f3f6f3014a288e6d05
Otello <otello.hanara@gmail.com>
2016-12-19 13:59:05 +0200

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Work on todo app',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Learn Node.JS',
-    completed: true
-}];
+
+//var todos = [{
+//    id: 1,
+//    description: 'Work on todo app',
+//    completed: false
+//}, {
+//    id: 2,
+//    description: 'Go to market',
+//    completed: false
+//}, {
+//    id: 3,
+//    description: 'Learn Node.JS',
+//    completed: true
+//}];
+
+app.use(bodyParser.json());
+
+var todos = [];
+var todoNextId = 1;
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -41,4 +48,14 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send('Todo not found.');
     }
     //res.send('Asking for todo with id of ' + req.params.id)
+});
+
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    
+    console.log('Description: ' + body.description);
+    
+    res.json(body);
 });
