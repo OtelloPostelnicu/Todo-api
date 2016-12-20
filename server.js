6b3f1094fe5fe45e85a4ff0307d92b4d5c910e28
Otello <otello.hanara@gmail.com>
2016-12-20 16:58:40 +0200

    Final static todos

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -40,7 +40,13 @@ app.get('/todos', function (req, res) {
         filteredTodos = _.where(filteredTodos, {completed: true});
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, {completed: false});
-    };
+    }
+    
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+            return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
+        });
+    }
     
     res.json(filteredTodos);
 });
